69db46af8f0c75a88ff104ec48b12556e4d432b3
Ayfer Şahin <ayfers547@gmail.com>
2024-09-13 00:28:18 +0300

    fetched data from table blog and categories

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using diziYorumSitesi.entity;
 namespace diziYorumSitesi
 {
     public partial class Default : System.Web.UI.Page
     {
+        diziYorumEntities db = new diziYorumEntities();
         protected void Page_Load(object sender, EventArgs e)
         {
+            var bloglar = db.TBL_BLOG.ToList();
+            Repeater1.DataSource = bloglar;
+            Repeater1.DataBind();
 
+            var bloglar2 = db.TBL_KATEGORI.ToList();
+            Repeater2.DataSource = bloglar2;
+            Repeater2.DataBind();
+
+            var bloglar3 = db.TBL_BLOG.ToList();
+            Repeater3.DataSource = bloglar3;
+            Repeater3.DataBind();
         }
     }
 }
