1afe0b55530d677acd4a92413b3222387c7ee050
Ayfer Şahin <ayfers547@gmail.com>
2024-09-18 15:37:14 +0300

    updating comments are done

---
+++ admin/yorumGuncelle.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/admin/admin.Master" AutoEventWireup="true" CodeBehind="yorumGuncelle.aspx.cs" Inherits="diziYorumSitesi.admin.yorumGuncelle" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form runat="server">
+
+        <asp:TextBox ID="txtID" runat="server" CssClass="form-control" placeholder="Yorum id" Enabled="false"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="txtKullanici" runat="server" CssClass="form-control" placeholder="Kullanıcı Adı" Enabled="false"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="txtBaslik" runat="server" CssClass="form-control" placeholder="Blog Başlık" Enabled="false" ></asp:TextBox>
+        <br />
+        <asp:TextBox ID="txtYorum" runat="server" CssClass="form-control" placeholder="Yorum" Height="100" TextMode="MultiLine"></asp:TextBox>
+        <br />
+
+
+
+        <br />
+        <asp:Button ID="btnKaydet" runat="server" Text="Kaydet" CssClass="btn btn-success" OnClick="btnKaydet_Click" />
+    </form>
+</asp:Content>
---
+++ admin/yorumGuncelle.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using diziYorumSitesi.entity;
+namespace diziYorumSitesi.admin
+{
+    public partial class yorumGuncelle : System.Web.UI.Page
+    {
+        diziYorumEntities db = new diziYorumEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                int id = Convert.ToInt32(Request.QueryString["YORUMID"]);
+                var yorum = db.TBL_YORUMLAR.Find(id);
+                txtBaslik.Text = yorum.TBL_BLOG.BLOGBASLIK;
+                txtID.Text = yorum.YORUMID.ToString();
+                txtKullanici.Text = yorum.KULLANICIADI;
+                txtYorum.Text = yorum.YORUMICERIK;
+            }
+           
+
+        }
+
+        protected void btnKaydet_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(Request.QueryString["YORUMID"]);
+            var yorum = db.TBL_YORUMLAR.Find(id);
+            yorum.YORUMICERIK = txtYorum.Text;
+            db.SaveChanges();
+            Response.Redirect("yorumlar.aspx");
+        }
+    }
+}
